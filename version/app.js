083cbbd37db2aa069b4ebeaed83c6ba56d4d145d
student <student@workstation.localhost>
2020-05-01 13:15:29 +0530

    update-app

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 3 of the app.' + '\n';
+    response = 'My 1st project deploy by Redhat OpenShif Container Platform with webhooks.' + '\n';
 
     //send the response to the client
     res.send(response);
